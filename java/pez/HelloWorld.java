cba428884f23ae955244f7164da9101de6ee2c98
Peter Strömberg <pez@pezius.com>
2022-01-01 17:45:10 +0100

    first commit

---
+++ java/pez/HelloWorld.java
@@ -0,0 +1,21 @@
+package pez;
+
+public class HelloWorld {
+    public static String FOO = "Foo";
+
+    public HelloWorld() {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+
+    public static String foo() {
+        return "foo";
+    }
+
+    public String bar() {
+        return "bar";
+    }
+}
---
+++ src/pez/java_hello.clj
@@ -0,0 +1,8 @@
+(ns pez.java-hello
+  (:import [pez HelloWorld]))
+
+(comment
+  HelloWorld/FOO
+  (HelloWorld/foo)
+  (.bar (HelloWorld.))
+  )
